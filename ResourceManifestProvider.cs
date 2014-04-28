a6b6fb7741051489e8df5e8f0c67050fcbf38ec0
Wojciech Markowski <markowsw@medicover.pl>
2014-04-28 16:02:57 +0200

    Changed include into Require to remove js duplication.

---
+++ ResourceManifestProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace PJS.Bootstrap
+{
+    using Orchard.UI.Resources;
+
+    public class ResourceManifestProvider : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+        }
+    }
+}
